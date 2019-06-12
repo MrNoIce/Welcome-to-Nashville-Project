f97f3397b501b79711a42edbeb5dbec4ee121628
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-06-12 15:48:17 -0500

    added button and everything shows up

--- src/scripts/concerts/concerts-API.js
+++ src/scripts/concerts/concerts-API.js
@@ -1,27 +1,42 @@
 
 
-let searchConcertInput = document.querySelector('#concertSearch')
+
+let searchInput = document.querySelector("#concertSearch")
+document.querySelector("#concertBtn").addEventListener("click", () => {
+    nameData(searchInput)
+})
+
+
 function nameData(name) {
 
-    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?city=Nashville&classificationId=KZFzniwnSyZfZ7v7nJ&apikey=kBa0lIh8vkJ1QB6WbS90QDIL7juNQ13B&keyword=${name}`)
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?city=Nashville&classificationId=KZFzniwnSyZfZ7v7nJ&apikey=kBa0lIh8vkJ1QB6WbS90QDIL7juNQ13B&keyword=${name.value}`)
 
         .then(response => response.json())
         .then(concertData => {
             let artistArray = concertData._embedded.events
             // console.log(artistArray)
 
-            for (let i = 0; i < artistArray.length; i++) {
 
-               var artistName = artistArray[i].name
+            for (let i = 0; i < 5; i++) {
 
-                // console.log(artistArray[i]._embedded.venues[0].name)
-                // console.log(artistArray[i]._embedded.venues[0].address.line1)
-                // console.log(artistArray[i].dates.start.localDate)
-            }
+                let artistName = artistArray[i].name
 
-        })
+                let venuesAddress = artistArray[i]._embedded.venues[0].name
+
+
+                for (let j = 0; j < 6; j++) {
+                    putInDOM = `<p${i}-content> ${artistName} : ${venuesAddress}</p> <button id= ${i}-btn type="button">I'm button${i}</button>
+            `
+         
+                }
+                document.querySelector("#searchResults").innerHTML += putInDOM
+               
+
+        }
+        
 }
+        )}
+        
+// document.querySelector("#${i}-btn").addEventListener("click", ()=>{
+//     document.querySelector("#itenirary").innerHTML += putInDOM
 
-nameData("john")
-
-console.log(artistName)
