69796cd16686110de6e9296c7361a237725bdfa1
Adam Knowles <acknowles524@gmail.com>
2019-06-13 14:19:08 -0500

    finished API and put divs in index.html

--- src/scripts/parkAPI.js
+++ src/scripts/parkAPI.js
@@ -1,6 +1,6 @@
 function nameData() {
   fetch(
-    `https://data.nashville.gov/resource/74d7-b74t.json?$$app_token=gLc22Eme02hYsyINkpeT7Z94W&walk_jog_paths=Yes`
+    `https://data.nashville.gov/resource/74d7-b74t.json?${keys.key}=${keys.token}&walk_jog_paths=Yes`
   )
     .then(response => response.json())
     .then(parkData => {
@@ -10,39 +10,36 @@ function nameData() {
 
       //for loop that loops through park data, adds HTML to dom
       for (i = 0; i < 5; i++) {
-        putInSearchResults.innerHTML += `<p id="idSearchResult${i}">${
-          parkData[i].park_name}</p>
+        putInSearchResults.innerHTML += `<p id=m"idSearchResult${i}">${
+          parkData[i].park_name
+        }</p>
           <button class="addPark" id="itenBtn--${i}">Add</button>`;
-          //event listener for the add button on the search results to add park name to itinerary
-          addToBtn()
-          
-        }
-      });
-    }
-    document.querySelector("#parkBtn").addEventListener("change", nameData);
-    
-    function addToIten (){
-      //this is wh
-      //here the itinerary is located/targeted
-      let putInItinerary = document.querySelector("#itenirary");
-        //this displays a park name in able to add name to the itinerary
-        let btnId = event.target.id 
-        let btnIdArray = btnId.split("--")
-        let btnIdNumber = btnIdArray[1]
-        let addParkName = document.querySelector("#idSearchResult" + btnIdNumber).textContent;
-        
-        //puts park name in the itinerary
-        
-          putInItinerary.innerHTML += `<p>${addParkName}</p>
+        //event listener for the add button on the search results to add park name to itinerary
+        addToBtn();
+      }
+    });
+}
+document.querySelector("#parkBtn").addEventListener("change", nameData);
+
+function addToIten() {
+  //here the itinerary is located/targeted
+  let putInItinerary = document.querySelector("#park");
+  //this displays a park name in able to add name to the itinerary
+  let btnId = event.target.id;
+  let btnIdArray = btnId.split("--");
+  let btnIdNumber = btnIdArray[1];
+  let addParkName = document.querySelector("#idSearchResult" + btnIdNumber)
+    .textContent;
+
+  //puts park name in the itinerary
+
+  putInItinerary.innerHTML = `<p>Park: ${addParkName}</p>
           `;
-        
-
-  console.log("button pressed")
 }
 
-function addToBtn (){
-let allAddBtns = document.querySelectorAll(".addPark")
-for (let i = 0; i < allAddBtns.length; i++) {
-  allAddBtns[i].addEventListener('click', addToIten )
-}
+function addToBtn() {
+  let allAddBtns = document.querySelectorAll(".addPark");
+  for (let i = 0; i < allAddBtns.length; i++) {
+    allAddBtns[i].addEventListener("click", addToIten);
+  }
 }
