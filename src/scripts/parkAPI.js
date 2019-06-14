400f3e311ac9a6b96f26ecb4acf2ad040d52a26a
Adam Knowles <acknowles524@gmail.com>
2019-06-13 23:41:54 -0500

    Added more park features in index.html and added functionality in parksAPI.js

--- src/scripts/parkAPI.js
+++ src/scripts/parkAPI.js
@@ -1,6 +1,6 @@
-function nameData() {
+function nameData(name) {
   fetch(
-    `https://data.nashville.gov/resource/74d7-b74t.json?${keys.key}=${keys.token}&walk_jog_paths=Yes`
+    `https://data.nashville.gov/resource/74d7-b74t.json?${keys.key}=${keys.token}&${name}`
   )
     .then(response => response.json())
     .then(parkData => {
@@ -12,15 +12,14 @@ function nameData() {
       //for loop that loops through park data, adds HTML to dom
       for (i = 0; i < 5; i++) {
         putInSearchResults.innerHTML += `<p id="idSearchResult${i}">${
-          parkData[i].park_name
-        }</p>
+          parkData[i].park_name}</p>
           <button class="addPark" id="itenBtn--${i}">save</button>`;
         //event listener for the add button on the search results to add park name to itinerary
         addToBtn();
       }
     });
 }
-document.querySelector("#parkBtn").addEventListener("change", nameData);
+// document.querySelector("#parkBtn").addEventListener("change", nameData);
 
 function addToIten() {
   //here the itinerary is located/targeted
@@ -43,4 +42,32 @@ function addToBtn() {
   for (let i = 0; i < allAddBtns.length; i++) {
     allAddBtns[i].addEventListener("click", addToIten);
   }
+}
+
+function fetchParkData(menu) {
+  if (menu.value == '2') {
+    nameData("hiking_trails=Yes")
+    
+}
+else if (menu.value == '3') {
+  nameData("basketball_courts=Yes")
+}
+else if(menu.value == '4') {
+  nameData("playground=Yes")
+}
+else if(menu.value == '5') {
+  nameData("disc_golf=Yes")
+}
+else if(menu.value == '6') {
+  nameData("nature_center=Yes")
+}
+else if(menu.value == '7') {
+  nameData("dog_park=Yes")
+}
+else if(menu.value == '8') {
+  nameData("lake=Yes")
+}
+else if(menu.value == '9') {
+  nameData("historic_features=Yes")
+}
 }
